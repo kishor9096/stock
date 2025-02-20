7fd46f5b8203fd1fcff2ae91547152fb832e8c7d
kishor9096 <kishor9096@gmail.com>
2025-02-20 12:30:10 +0530

    updated

--- run_max_pain_sensibull.sh
+++ run_max_pain_sensibull.sh
@@ -1,6 +1,6 @@
 # filepath: /home/ubuntu/LLM/stock/run_max_pain_sensibull.sh
 #!/bin/bash
 source /home/ubuntu/LLM/stock/.venv/bin/activate
-/home/ubuntu/LLM/stock/.venv/bin/python /home/ubuntu/LLM/stock/max_pain_sensibull.py >> /home/ubuntu/LLM/stock/cron_log.txt 2>&1
+/home/ubuntu/LLM/stock/.venv/bin/python /home/ubuntu/LLM/stock/max_pain_sensibull.py >> /home/ubuntu/LLM/stock/max_pain_sensibull_cron_log.txt 2>&1
 
 /home/ubuntu/LLM/stock/.venv/bin/python /home/ubuntu/LLM/stock/moneyControlScraping.py >> /home/ubuntu/LLM/stock/moneyControlScraping_cron_log.txt 2>&1
--- run_scheduler.sh
+++ run_scheduler.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 # to fetch latest news on scheduled basis
 source /home/ubuntu/LLM/TechunarStocksAI/.venv/bin/activate
-/home/ubuntu/LLM/TechunarStocksAI/.venv/bin/python /home/ubuntu/LLM/TechunarStocksAI/ExtractNews.py.py >> /home/ubuntu/LLM/TechunarStocksAI/cron_log.txt 2>&1
+/home/ubuntu/LLM/TechunarStocksAI/.venv/bin/python /home/ubuntu/LLM/TechunarStocksAI/ExtractNews.py >> /home/ubuntu/LLM/TechunarStocksAI/ExtractNews_cron_log.txt 2>&1
 cd /home/ubuntu/immich/immich-app
 docker compose pull && docker compose up -d
 docker image prune
